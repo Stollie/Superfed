efa0e7e6c6a393c2ca75ebce6f94ec3d38252b3f
Randy van Vugt <randyvanvugt@gmail.com>
2011-12-05 20:45:22 +0100

    Taken App: Panels, List, Details
    Heb de details pagina aan de praat gekregen en heb deze maar meteen in
    de 'Taken' App verwerkt.

--- sencha-touch-1.1.1/apps/taken/app/taken.js
+++ sencha-touch-1.1.1/apps/taken/app/taken.js
@@ -1,30 +1,134 @@
 // JavaScript Document
 
-	new Ext.Application({
+TakenApp = new Ext.Application({
+	
+	name: 'Taken App',
+	launch: function() {
 		
-		name: 'Taken',
-		launch: function() {
-		
-			new Ext.TabPanel({
-				
-				fullscreen: true,
-				dockedItems: [
-					
-					{
-					xtype:'toolbar', 
-					title:'Taken',
-					dock:'top' 
-					},
-					{
-					xtype:'toolbar', 
-					title:'Das Knopjens jah!',
-					dock:'bottom' 
-					}
-				
-				]
-								
-			});
+		/* Panel: Toevoegen */
+		TakenApp.Addcard = new Ext.Panel({
 			
-		}
+			title: "Toevoegen",
+			iconCls: "add",
+			html: 'Todo: Add'
+			
+		});
 		
-	});
+		/* List: Voltooid */
+		TakenApp.Donecard = new Ext.List({
+		
+			title: "Voltooid",
+			iconCls: "list",
+			id: 'donelist',
+			store: TakenApp.DoneStore,
+			itemTpl: '<div class="contact">[ V ] {title}</div>'
+			
+		});
+		
+		/* Panel: Informatie */
+		TakenApp.Infocard = new Ext.Panel({
+			
+			title: "Informatie",
+			iconCls: "info",
+			html: 'Todo: Info'
+			
+		});
+		
+		
+		
+		/* List: Onvoltooide */
+		TakenApp.Todolist = new Ext.List({
+			
+			title: "Onvoltooid",
+			iconCls: "list",
+			id: 'todolist',
+			store: TakenApp.TodoStore,
+			itemTpl: '<div class="contact">[   ] {title}</div>',
+			onItemDisclosure: function(record) { 
+			
+				TakenApp.detailsToolbar.setTitle(record.data.title);
+				TakenApp.Tododetails.update(record.data);
+				TakenApp.Todocard.setActiveItem('tododetails');
+		
+			},
+		
+		});
+		
+		/* Panel: listWrapper */
+		TakenApp.listWrapper = new Ext.Panel({
+			
+			id: 'listwrapper',
+			layout: 'fit',
+			items: [TakenApp.Todolist],
+			dockedItems: [{
+				xtype: 'toolbar',
+				title: 'Onvoltooide Taken'
+			}]
+			
+		});
+
+		/* Toolbar: details */
+		TakenApp.detailsToolbar = new Ext.Toolbar({
+			
+			items: [{
+				text: 'Terug',
+				uit: 'back',
+				handler: function() {
+					TakenApp.Todocard.setActiveItem('listwrapper',{ type:'slide',direction:'right' })	
+				}
+			}]
+			
+		});
+		
+		/* Panel: Details */
+		TakenApp.Tododetails = new Ext.Panel({
+			
+			id: 'tododetails',
+			tpl: '[  ] {title}',
+			dockedItems: [TakenApp.detailsToolbar]
+			
+		
+		});
+		
+		/* Panel: Onvoltooid */
+		TakenApp.Todocard = new Ext.Panel({
+			
+			title: "Taken",
+			iconCls: "list",
+			
+			fullscreen: true,
+			layout: 'card',
+			cardSwitchAnimation: 'slide',
+		
+			items: [TakenApp.listWrapper, TakenApp.Tododetails]	
+			
+			
+		});
+
+		/* Panel: Main */
+		new Ext.TabPanel({
+	
+			fullscreen: true,
+				
+			tabBar: {
+				
+				dock: 'bottom',
+				layout: {
+					pack: 'center'	
+				}
+				
+			},
+			
+			defaults: {
+				
+				styleHtmlContent: true
+				
+			},
+			
+			items: [ TakenApp.Todocard, TakenApp.Addcard, TakenApp.Donecard, TakenApp.Infocard ]
+			
+		});
+		
+	}	
+	
+});
